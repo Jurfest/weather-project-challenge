3b36067a01ca58965e6977ce901912bc60393bb9
IgorFornazieri <ygor0504@yahoo.com.br>
2020-04-14 11:00:23 -0300

    inicial design

--- src/app/weather-home/weather-home.component.ts
+++ src/app/weather-home/weather-home.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherHomeComponent implements OnInit {
 
+  locale:string = "SÃO PAULO"
+  
+  hour:string = "MANHA"
+
+  weather:string = "CALOR"
+
+  temp:string = "12º"
+
+  humidity:string = "67%"
+
+  wind:string = "12 K/M"
+  
+  
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
