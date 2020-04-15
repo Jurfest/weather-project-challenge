5e82cff7f301e9ee9a9b4584d8b3c97ca9620d64
IgorFornazieri <ygor0504@yahoo.com.br>
2020-04-15 13:34:56 -0300

    get locale, temp, wind and humidity from api

--- src/app/weather-search/weather-search.component.ts
+++ src/app/weather-search/weather-search.component.ts
@@ -21,17 +21,22 @@ export class WeatherSearchComponent implements OnInit {
   entries:any;
   weatherKeys:any;
   weatherValues:any;
+  locale:any
+  teste:any
   callApi() {
     let weatherData;
     this.apiservice.getData("Raleigh").subscribe(
       (data) => {
         weatherData = new Object(data);
-        this.entries = Object.entries(weatherData.data[0]);
-        this.weatherKeys = Object.keys(weatherData.data[0]);
-        this.weatherValues = Object.values(weatherData.data[0]);
+        this.locale = weatherData.data[0].city_name
+        this.entries = weatherData.data[0].temp;
+        this.weatherKeys = weatherData.data[0].wind_spd;
+        this.weatherValues = weatherData.data[0].rh;
+        
+        console.log(this.locale)
         console.log(this.entries)
-        console.log(this.weatherValues)
         console.log(this.weatherKeys)
+        console.log(this.weatherValues)
       }
     );
     
