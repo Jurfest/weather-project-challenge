add01da5764af7c63d8bb1f047d9cd1b80775708
lookfire <renancontiero@hotmail.com>
2020-04-16 11:20:21 -0300

    gerado o model weather-obj.ts

---
+++ src/app/models/weather-obj.spec.ts
@@ -0,0 +1,7 @@
+import { WeatherObj } from './weather-obj';
+
+describe('WeatherObj', () => {
+  it('should create an instance', () => {
+    expect(new WeatherObj()).toBeTruthy();
+  });
+});
---
+++ src/app/models/weather-obj.ts
@@ -0,0 +1,22 @@
+export class WeatherObj {
+    constructor(
+      public city_name: string,
+      public temp: number,
+      public wind:number,
+      public moisture:number,
+      public city_img?: string
+    ) {}
+}
+// locale:string = "São Paulo"
+  
+//   hour:string = "Manha"
+
+//   weather:string = "Calor"
+
+//   temp:string = "12";
+//   max_temp: string = "32º"
+//   min_temp: string = "10º"
+
+//   humidity:string = "67%"
+
+//   wind:string = "12 K/M"
